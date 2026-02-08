71dd7d18c8a8d0e8ad1a56231b60d95040c8b84f
LucasLdp <lucasdepauloldp1@gmail.com>
2026-02-08 19:52:05 -0300

    feat: add FindByIdUserQuery class and update query exports

--- backend/src/modules/users/application/queries/query/findById-user.query.ts
+++ backend/src/modules/users/application/queries/query/findById-user.query.ts
@@ -1,7 +1,7 @@
 import { User } from '@/modules/users/domain/entities/user.entity';
 import { Query } from '@nestjs/cqrs';
 
-export class FindOneUserQuery extends Query<User> {
+export class FindByIdUserQuery extends Query<User> {
   constructor(public readonly id: string) {
     super();
   }
--- backend/src/modules/users/application/queries/query/index.ts
+++ backend/src/modules/users/application/queries/query/index.ts
@@ -1,1 +1,3 @@
 export * from './listAll-user.query';
+export * from './findById-user.query';
+export * from './findByEmail-user.query';
