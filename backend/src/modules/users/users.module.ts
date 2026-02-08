08c16ed73895d461ec223afaf1fcb031f80c42c8
LucasLdp <lucasdepauloldp1@gmail.com>
2026-02-08 20:15:17 -0300

    feat: add UsersController and UsersService unit tests

---
+++ backend/src/modules/users/users.module.ts
@@ -0,0 +1,31 @@
+import { Module, Provider } from '@nestjs/common';
+import { UsersController } from './presentation/http/users.controller';
+import * as UserCommands from '@modules/users/application/commands/handler';
+import * as UserQueries from '@modules/users/application/queries/handler';
+import { PrismaUserRepository } from './infra/database/prisma-user.repository';
+import { UserRepository } from './domain/repositories/user.repository';
+
+const UserCommandHandlers = [
+  UserCommands.CreateUserUseCase,
+  UserCommands.UpdateUserUseCase,
+  UserCommands.DeleteUserHandler,
+];
+
+const UserQueryHandlers = [
+  UserQueries.FindAllUsersUseCase,
+  UserQueries.FindByIdUserHandler,
+  UserQueries.FindUserByEmailHandler,
+];
+
+const UserProvider: Provider[] = [
+  {
+    provide: UserRepository,
+    useClass: PrismaUserRepository,
+  },
+];
+
+@Module({
+  controllers: [UsersController],
+  providers: [...UserCommandHandlers, ...UserQueryHandlers, ...UserProvider],
+})
+export class UsersModule {}
