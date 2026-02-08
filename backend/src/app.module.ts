d3c620c3718640950f15bd329beddbdf51d16356
LucasLdp <lucasdepauloldp1@gmail.com>
2026-02-08 20:15:21 -0300

    feat: add Zod validation and serialization support in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,80 @@
-import { Module } from '@nestjs/common';
+import {
+  ZodValidationPipe,
+  ZodSerializerInterceptor,
+  ZodSerializationException,
+} from 'nestjs-zod';
+import {
+  APP_PIPE,
+  APP_INTERCEPTOR,
+  APP_FILTER,
+  BaseExceptionFilter,
+} from '@nestjs/core';
+import { ZodError } from 'zod';
+import {
+  Module,
+  HttpException,
+  ArgumentsHost,
+  Logger,
+  Catch,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { validateEnvFunction } from '@/config/env-validation';
+import { PrismaModule } from 'nestjs-prisma';
+import { CqrsModule } from '@nestjs/cqrs';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Catch(HttpException)
+class HttpExceptionFilter extends BaseExceptionFilter {
+  private logger = new Logger(HttpExceptionFilter.name);
+
+  catch(exception: HttpException, host: ArgumentsHost) {
+    if (exception instanceof ZodSerializationException) {
+      const zodError = exception.getZodError();
+
+      if (zodError instanceof ZodError) {
+        this.logger.error(`ZodSerializationException: ${zodError.message}`);
+      }
+    }
+
+    super.catch(exception, host);
+  }
+}
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validate: validateEnvFunction,
+    }),
+    PrismaModule.forRoot({
+      prismaServiceOptions: {
+        prismaOptions: {
+          adapter: new PrismaPg({
+            connectionString: process.env.DATABASE_URL,
+          }),
+        },
+      },
+    }),
+    CqrsModule.forRoot(),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ZodSerializerInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
