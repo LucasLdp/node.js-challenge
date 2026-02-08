7ca8889f3dfc8ac1aa9867bbefc4aba9649a2923
LucasLdp <lucasdepauloldp1@gmail.com>
2026-02-08 20:10:49 -0300

    feat: refactor CreateUserCommand structure and add DeleteUserCommand

--- backend/src/modules/users/application/commands/command/create-user.command.ts
+++ backend/src/modules/users/application/commands/command/create-user.command.ts
@@ -2,9 +2,11 @@ import { Command } from '@nestjs/cqrs';
 
 export class CreateUserCommand extends Command<void> {
   constructor(
-    public readonly name: string,
-    public readonly email: string,
-    public readonly password: string,
+    public data: {
+      name: string;
+      email: string;
+      password: string;
+    },
   ) {
     super();
   }
