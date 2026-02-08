cd51485c9595650b48e6b77bd13bb9ea98916950
LucasLdp <lucasdepauloldp1@gmail.com>
2026-02-08 19:36:23 -0300

    feat: implement CreateUserUseCase and UpdateUserUseCase for user management

---
+++ backend/src/modules/users/application/commands/handler/create-user.handler.ts
@@ -0,0 +1,29 @@
+import { UserRepository } from '@modules/users/domain/repositories/user.repository';
+import { CreateUserCommand } from '@modules/users/application/commands/command';
+import { User } from '../../../domain/entities/user.entity';
+import { ConflictException } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+
+@CommandHandler(CreateUserCommand)
+/**
+ * Use case para criar um novo usuário. Verifica se o email já está em uso e, se não estiver, cria um novo usuário com os dados fornecidos.
+ *  */
+export class CreateUserUseCase implements ICommandHandler<CreateUserCommand> {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute({ name, email, password }: CreateUserCommand): Promise<void> {
+    const existingUser = await this.userRepository.findByEmail(email);
+
+    if (existingUser) {
+      throw new ConflictException('Email já está em uso.');
+    }
+
+    const user = new User({
+      name,
+      email,
+      password,
+    });
+
+    await this.userRepository.create(user);
+  }
+}
---
+++ backend/src/modules/users/application/commands/handler/update-user.handler.ts
@@ -0,0 +1,30 @@
+import { UserRepository } from '@modules/users/domain/repositories/user.repository';
+import { UpdateUserCommand } from '@modules/users/application/commands/command';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+
+/**
+ * Use case para atualizar um usuário existente. Verifica se o email fornecido já está em uso por outro usuário e se o usuário a ser atualizado existe. Se as validações passarem, atualiza os dados do usuário com as informações fornecidas.
+ *  */
+@CommandHandler(UpdateUserCommand)
+export class UpdateUserUseCase implements ICommandHandler<UpdateUserCommand> {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute({ id, data }: UpdateUserCommand): Promise<void> {
+    if (data.email) {
+      const userWithEmail = await this.userRepository.findByEmail(data.email);
+
+      if (userWithEmail && userWithEmail.id !== id) {
+        throw new ConflictException('Email já está em uso.');
+      }
+    }
+
+    const existingUser = await this.userRepository.findById(id);
+
+    if (!existingUser) {
+      throw new NotFoundException('Usuário não encontrado.');
+    }
+
+    await this.userRepository.update(id, data);
+  }
+}
