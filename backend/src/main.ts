88920cd0a5795ca46393f178ca1e739fbd178023
LucasLdp <lucasdepauloldp1@gmail.com>
2026-02-08 21:28:00 -0300

    feat: update API title and description in Swagger documentation and configure Zod for Portuguese locale

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { cleanupOpenApiDoc } from 'nestjs-zod';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import z from 'zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,11 +10,13 @@ async function bootstrap() {
   const openApiDoc = SwaggerModule.createDocument(
     app,
     new DocumentBuilder()
-      .setTitle('Example API')
-      .setDescription('Example API description')
+      .setTitle('Kinvo Challenge API')
+      .setDescription('API for Kinvo Challenge')
       .setVersion('1.0')
       .build(),
   );
+
+  z.config(z.locales.pt());
 
   SwaggerModule.setup('api', app, cleanupOpenApiDoc(openApiDoc));
   await app.listen(process.env.PORT ?? 3000);
