566f0d5250a040d266f2f94a24b984a03b139e74
LucasLdp <lucasdepauloldp1@gmail.com>
2026-02-10 10:51:41 -0300

    feat: create AuthModule with JWT authentication setup

---
+++ backend/src/modules/auth/auth.module.ts
@@ -0,0 +1,34 @@
+import { AuthService } from '@/modules/auth/application/services/auth.service';
+import { JwtAuthGuard } from '@/modules/auth/infra/guards/jwt-auth.guard';
+import { AuthController } from '@/modules/auth/presentation/http/auth.controller';
+import { UsersModule } from '@/modules/users/users.module';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRES_IN') ?? '7d',
+        },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
+  exports: [AuthService],
+})
+export class AuthModule {}
