aac6f8e3f753508395276eeec97d2d8700bdf34d
LucasLdp <lucasdepauloldp1@gmail.com>
2026-02-10 10:02:26 -0300

    fix: update import path for CashFlow entity in CashFlowFactory

--- backend/test/factories/cash-flow.factory.ts
+++ backend/test/factories/cash-flow.factory.ts
@@ -1,4 +1,4 @@
-import { CashFlow } from '@/cash-flows/domain/entities/cash-flow.entity';
+import { CashFlow } from '@/modules/cash-flows/domain/entities/cash-flow.entity';
 import { faker } from '@faker-js/faker';
 import { createId } from '@paralleldrive/cuid2';
 
@@ -12,6 +12,7 @@ export class CashFlowFactory {
       amount: faker.number.float({ min: 1, max: 10000, fractionDigits: 2 }),
       type: faker.helpers.arrayElement(['INCOME', 'EXPENSE']),
       description: faker.commerce.productDescription(),
+      date: faker.date.recent(),
       id: createId(),
       ...attrs,
     });
