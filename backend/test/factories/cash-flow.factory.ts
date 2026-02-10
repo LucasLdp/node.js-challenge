9d89751ad8eb2dbc079f670389366b7a2c3b835f
LucasLdp <lucasdepauloldp1@gmail.com>
2026-02-10 01:27:15 -0300

    feat: add CashFlowFactory for creating CashFlow instances in tests

---
+++ backend/test/factories/cash-flow.factory.ts
@@ -0,0 +1,26 @@
+import { CashFlow } from '@/cash-flows/domain/entities/cash-flow.entity';
+import { faker } from '@faker-js/faker';
+import { createId } from '@paralleldrive/cuid2';
+
+/**
+ * Factory class for creating CashFlow instances for testing purposes.
+ *  */
+export class CashFlowFactory {
+  static create(attrs: Partial<CashFlow['props']> = {}): CashFlow {
+    return new CashFlow({
+      userId: createId(),
+      amount: faker.number.float({ min: 1, max: 10000, fractionDigits: 2 }),
+      type: faker.helpers.arrayElement(['INCOME', 'EXPENSE']),
+      description: faker.commerce.productDescription(),
+      id: createId(),
+      ...attrs,
+    });
+  }
+
+  static createMany(
+    count: number,
+    attrs: Partial<CashFlow['props']> = {},
+  ): CashFlow[] {
+    return Array.from({ length: count }, () => this.create(attrs));
+  }
+}
