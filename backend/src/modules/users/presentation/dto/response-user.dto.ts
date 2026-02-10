e72e924679003b470cba2e2f0e01d201cdc2ac43
LucasLdp <lucasdepauloldp1@gmail.com>
2026-02-10 11:45:10 -0300

    fix: change user ID type from uuid to cuid in UserResponseSchema

--- backend/src/modules/users/presentation/dto/response-user.dto.ts
+++ backend/src/modules/users/presentation/dto/response-user.dto.ts
@@ -2,7 +2,7 @@ import { createZodDto } from 'nestjs-zod';
 import z from 'zod';
 
 const UserResponseSchema = z.object({
-  id: z.uuid(),
+  id: z.cuid(),
   name: z.string(),
   email: z.email(),
   createdAt: z.iso.datetime(),
