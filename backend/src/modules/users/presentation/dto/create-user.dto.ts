4287aec2271821264cd45baf730a2f41c0f600ae
LucasLdp <lucasdepauloldp1@gmail.com>
2026-02-11 00:27:35 -0300

    refactor: change role optional value

--- backend/src/modules/users/presentation/dto/create-user.dto.ts
+++ backend/src/modules/users/presentation/dto/create-user.dto.ts
@@ -6,6 +6,7 @@ const CreateUserSchema = z.object({
   name: z.string().min(1, 'Name is required').describe('Nome do usuário'),
   email: z.email('Invalid email address').describe('Email do usuário'),
   password: z.string().describe('Senha do usuário'),
+  role: z.enum(['ADMIN', 'USER']).optional().describe('Papel do usuário'),
 });
 
 export class CreateUserDto extends createZodDto(CreateUserSchema) {}
